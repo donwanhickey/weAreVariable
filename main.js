27e026a7116e7aca1b5601c643e9ef454e7cfa76
unknown <donwanhickey@gmail.com>
2025-05-15 21:57:08 -0400

    last commit

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
 /******************
  * YOUR CODE BELOW *
  ******************/
+const myFirstName = "Towanna";
+const myLastName = "Hickey";
+const myFavoriteCeleb = "Dante Hickey";
+const mySentence = "Towanna Hickey Dante Hickey";
+const myLuckyNum = 14;
+const myAge = 47;
+const myHeight = 5.7;
+const myFavoriteYear = 1978;
+let mySum = 5 + 2317 + 36; 2358;
+const myProduct = 232 * 43 / 2 + 4;  4996
+const myHeightAndAge = age = 47; myHeight = 5.7; myHeightAndAge = age + myHeight  
+
+
+
 
 
 /*********************************************************************************************************************
